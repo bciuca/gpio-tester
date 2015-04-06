586a5561e25652bd633c59c58de096cc7654c569
Bogdan Ciuca <bogdan.ciuca@gmail.com>
2015-04-05 23:26:46 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,89 @@
+'use strict';
+
+var restify = require('restify');
+var Gpio = require('onoff').Gpio;
+var server = restify.createServer();
+var io = require('socket.io')(8081);
+var fs = require('fs');
+var watchers = {};
+var exported = {};
+
+server.get(/\/?.*/, restify.serveStatic({
+    directory: './public',
+    default: 'index.html'
+}));
+
+
+function setGpio(options) {
+    var pin = parseInt(options.pin, 10);
+    var value = +options.value;
+    var gpio = new Gpio(pin, 'out');
+    gpio.writeSync(value);
+    exported[pin] = gpio;
+    console.log('wrote to %d with val %d', pin, value);
+}
+
+function watch(pin) {
+    console.log('watch pin %d', pin);
+    var gpio = new Gpio(pin, 'in', 'both');
+    gpio.watch(function(err, value) {
+        console.log('pin %d triggered %d', pin, value);
+        if (err) return console.error(err);
+
+        io.emit('gpio', { pin: pin, value: value });
+    });
+
+    watchers[pin] = watchers[pin] || gpio;
+}
+
+function unwatch(pin) {
+    console.log('unwatch pin %d', pin);
+    watchers[pin] && watchers[pin].handler && watchers[pin].handler.unexport();
+}
+
+function exit() {
+    var exportedPins;
+    var pin;
+    var unexport;
+
+    console.log('process quitting, cleaning up ...');
+
+    try {
+        exportedPins = fs.readdirSync('/sys/class/gpio');
+        exportedPins && exportedPins.forEach(function(name) {
+            pin = name.match(/gpio(\d+)/);
+            if (pin && pin[1]) {
+                unexport = pin[1];
+                console.log('unexport pin %s', unexport);
+                fs.writeFileSync('/sys/class/gpio/unexport', unexport);
+            }
+        });
+    } catch (err) { 
+        console.error(err); 
+    }
+    
+    process.exit();
+}
+
+io.sockets.on('connection', function (socket) {
+    console.log('web socket connected');
+    
+    socket.on('ping', function (data) {
+        console.log(data);
+    });
+
+    socket.on('gpio', function(state) {
+        console.log('gpio', state);
+        state.dir === 'in' && watch(state.pin);
+    });
+
+    socket.on('gpio-write', function(state) {
+        console.log('gpio-write', state);
+        setGpio(state);
+    });
+
+});
+
+process.on('SIGINT', exit);
+process.on('SIGTERM', exit);
+server.listen(8080);
---
+++ public/main.js
@@ -0,0 +1,74 @@
+(function() {
+    'use strict';
+
+    window.onload = function() {
+        var gpio = {};
+        var gpioParent = document.getElementById('gpios');
+        var listeners = {};
+        var socket = io('http://' + window.location.hostname + ':8081');
+        
+        socket.on('connect', function() { 
+            console.log('web socket connected');
+            socket.emit('ping', {status: 'ok'});
+        });
+
+        socket.on('gpio', function(state) { 
+            console.log('gpio', state);
+            listeners[state.pin] && listeners[state.pin].handler && listeners[state.pin].handler(state)
+        });
+
+        socket.on('disconnect', function() {
+            console.log('socket disconnected');
+        });
+
+        gpioParent.addEventListener('click', function clickHandler(ev) {
+            var target = ev.target;
+            var parent = ev.target.parentNode.parentNode;
+            var inputs = ev.target.parentNode.querySelectorAll('input');
+            var direction = inputs[0].checked ? 'in' : inputs[1].checked ? 'out' : 'none';
+            var isOn = inputs[2].checked;
+            var pin = parseInt(parent.id.substring(4));
+
+            console.log('Status %s is on %s, pin %s', direction, isOn, pin);
+            
+            // trigger a pin write
+            if (!isNaN(pin) && target.type === 'checkbox' && direction === 'out') {
+                socket.emit('gpio-write', { pin: pin, value: +isOn, dir: direction });
+            }
+
+            // watch/unwatch pin
+            if (!isNaN(pin) && target.type === 'radio') {
+                console.log('emit', pin, +isOn, direction);
+                socket.emit('gpio', { pin: pin, value: +isOn, dir: direction });
+                if (direction === 'in') {
+                    watch(pin, parent);
+                } else {
+                    unwatch(pin);
+                }
+            }
+        });
+
+        function watch(pin, parentEl, cb) {
+            var listener = {
+                pin: pin,
+                parentEl: parentEl,
+                handler: function(state) {
+                    console.log('handling pin', listener.pin);
+                    parentEl.querySelector('span').classList[state.value ? 'add' : 'remove']('gpio-on');
+                }
+            };
+
+            listeners[pin] = listener;
+        }
+
+        function unwatch(pin) {
+            var listener = listeners[pin];
+            if (!listener) return;
+            
+            listener.parentEl && listener.parentEl.classList.remove('gpio-on');
+            
+            delete listeners[pin];
+        }
+    };
+
+})();
